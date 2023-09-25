f7ed8f28161a5064c03f07623208e208f49e3b83
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-25 12:01:03 -0400

    adding form validations with yup

--- 04-interes-compuesto/src/App.jsx
+++ 04-interes-compuesto/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
 import Input from './components/Input'
 import Button from './components/Button'
 import Container from './components/Container'
@@ -46,13 +47,33 @@ function App() {
             rate: '',
           }}
           onSubmit={handleSubmit}
+          validationSchema={Yup.object({
+            deposit: Yup.number()
+              .required('Obligatorio')
+              .typeError('Debe ser un número')
+              .min(1, 'El valor mínimo es 1'),
+            contribution: Yup.number()
+              .required('Obligatorio')
+              .typeError('Debe ser un número')
+              .min(1, 'El valor mínimo es 1'),
+            years: Yup.number()
+              .required('Obligatorio')
+              .typeError('Debe ser un número')
+              .min(1, 'El valor mínimo es 1')
+              .integer('Debe ser un número entero'),
+            rate: Yup.number()
+              .required('Obligatorio')
+              .typeError('Debe ser un número')
+              .min(0, 'El valor mínimo es 0')
+              .max(1, 'El Valor máximo es 1'),
+          })}
         >
           <Form>
             <Input name='deposit' label='Deposito inicial' />
             <Input name='contribution' label='Contribución anual' />
             <Input name='years' label='Años' />
             <Input name='rate' label='Interés estimado' />
-            <Button>Calcular</Button>
+            <Button type='submit'>Calcular</Button>
           </Form>
         </Formik>
         {balance !== '' ? <Balance>Balance final: {balance}</Balance> : null}
