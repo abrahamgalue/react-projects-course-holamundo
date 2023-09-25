f1e779357e4135f00b06d4a110c64983ba3221c1
Abraham Galue <abrahamgalue512@gmail.com>
2023-09-25 12:14:30 -0400

    separate logic into a file

--- 04-interes-compuesto/src/App.jsx
+++ 04-interes-compuesto/src/App.jsx
@@ -6,22 +6,7 @@ import Button from './components/Button'
 import Container from './components/Container'
 import Section from './components/Section'
 import Balance from './components/Balance'
-
-const compoundInterest = (deposit, contribution, years, rate) => {
-  let total = deposit
-  for (let i = 0; i < years; i++) {
-    total = (total + contribution) * (rate + 1)
-  }
-
-  return Math.round(total)
-}
-
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
-  maximumFractionDigits: 2,
-})
+import { compoundInterest, formatter } from './logic/utils'
 
 function App() {
   const [balance, setBalance] = useState('')
---
+++ 04-interes-compuesto/src/logic/utils.js
@@ -0,0 +1,15 @@
+export const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for (let i = 0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+
+  return Math.round(total)
+}
+
+export const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
