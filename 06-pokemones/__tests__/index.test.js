b2b66eb012e5ce4bbe2186e13005401909197828
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-27 12:41:01 -0400

    Test to verify dynamic components added

--- 06-pokemones/__tests__/index.test.js
+++ 06-pokemones/__tests__/index.test.js
@@ -3,14 +3,24 @@ import Index from '../pages/index'
 
 describe('Index', () => {
 
+  /* Podemos tener varios test anidados dentro de nuestro archivo */
   describe('Component', () => {
     it('se renderiza', () => {
       render(
-        <Index pokemones={[]} />
+        <Index pokemones={[{ name: 'Chanchito feliz', url: 'pokemon/detalle/1' }]} />
       )
 
+      /* screen nos permite acceder a todos los elementos de la página */
       const paragraph = screen.getByTestId('titulo')
       expect(paragraph).toBeInTheDocument()
+
+      const chanchito = screen.getByText('Chanchito feliz')
+      // console.log(chanchito.getAttribute('href'))
+
+      expect(chanchito).toBeInTheDocument()
+
+      const url = chanchito.getAttribute('href')
+      expect(url).toEqual('/pokemones/1')
     })
   })
 })
