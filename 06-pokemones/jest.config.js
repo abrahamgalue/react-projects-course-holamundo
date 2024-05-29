d1b1e7c560a86c669778c87a6897116fe130d6ef
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-29 14:27:21 -0400

    Install dependencies for testing (next-14)

---
+++ 06-pokemones/jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require('next/jest')
+
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+}
+
+module.exports = createJestConfig(config)
