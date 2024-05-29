3c0179643d4c74abad86c51e4c11007bcf451c20
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-29 13:51:51 -0400

    styled-components added to the project (next-14)

--- 06-pokemones/src/app/layout.js
+++ 06-pokemones/src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import StyledComponentsRegistry from "./lib/registry";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
