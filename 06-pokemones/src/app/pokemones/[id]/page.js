c5286b109968f97ad1f8dd005e80fed0bc37da5e
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-29 11:23:16 -0400

    Added pokemon information and off-domain images (next-14)

--- 06-pokemones/src/app/pokemones/[id]/page.js
+++ 06-pokemones/src/app/pokemones/[id]/page.js
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import Link from "next/link"
+
 async function getPokemones(id) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
   const data = await res.json()
@@ -6,10 +9,14 @@ async function getPokemones(id) {
 }
 
 export default async function Pokemon({ params: { id } }) {
-  const data = getPokemones(id)
+  const data = await getPokemones(id)
   console.log(data)
 
   return (
-    <p>lala</p>
+    <div>
+      <h1>{data.name} número #{data.id}</h1>
+      <Image src={data.sprites.front_default} width={400} height={400} alt={`Pokemon ${data.name}`} />
+      <Link href='/'>Volver al inicio</Link>
+    </div>
   )
 }
