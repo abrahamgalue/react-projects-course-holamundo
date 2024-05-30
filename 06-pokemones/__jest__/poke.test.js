3672f5d793d01020d1885477ff31435f5cc02a3e
Abraham Galue <abrahamgalue512@gmail.com>
2024-05-30 11:38:44 -0400

    Test to check the hooks of a created component (next-14)

---
+++ 06-pokemones/__jest__/poke.test.js
@@ -0,0 +1,31 @@
+import { screen, render, waitFor } from "@testing-library/react"
+import Poke from '../src/app/poke/page'
+
+describe('Poke', () => {
+  it('renders pokemones', async () => {
+    const mockResults = [{ name: 'chanchito', url: 'https://www.dominio.com/pokemones/1' }]
+
+    global.fetch = jest.fn()
+      .mockImplementation(url => {
+        return new Promise(resolve => {
+          resolve({
+            json: () => Promise.resolve({
+              results: mockResults
+            })
+          })
+        })
+      })
+
+    render(
+      <Poke />
+    )
+
+    const loading = screen.getByText('Cargando...')
+    expect(loading).toBeInTheDocument()
+    await waitFor(() => screen.getByText('Mi App de Pokemones'))
+    const element = screen.getByTestId(1)
+    const anchor = element.children[0]
+    expect(anchor).toHaveAttribute('href', '/pokemones/1')
+    expect(anchor).toHaveTextContent('chanchito')
+  })
+})
