96ad09d57cb4dd6799aadec1a60a3be5907ce77c
Abraham Galue <abrahamgalue512@gmail.com>
2024-06-01 10:44:30 -0400

    Phone design solved in the compound interest calculator project

--- 04-interes-compuesto/src/components/Section.jsx
+++ 04-interes-compuesto/src/components/Section.jsx
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 const Section = styled.section`
   background-color: #eee;
   border-top: 2px solid palevioletred;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+  margin: 20px;
   padding: 20px 25px;
   width: 500px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
 `
 
 export default Section
